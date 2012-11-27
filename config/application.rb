7cff28587fbdb6dc9c2337dd1b3e282cf1323de5
Nan Zhong <nan@nine27.com>
2012-11-27 14:59:38 -0500

    Don't initialize app on assets precompile

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module RefactrIt
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Don't initizalize app on precompile
+    config.assets.initialize_on_precompile = false
   end
 end
