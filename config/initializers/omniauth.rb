8688f8bc8fd748d0c462d6e880fe6dcdd256594f
Nan Zhong <nan@nine27.com>
2012-11-30 14:38:10 -0500

    Add user scope to github omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, 'd31c3e7336ddbecb2268', '87e7caaefc3fdca1fc89a0e60ace4b23b6fd429c'
+  provider :github, 'd31c3e7336ddbecb2268', '87e7caaefc3fdca1fc89a0e60ace4b23b6fd429c', scope: 'user'
 end
