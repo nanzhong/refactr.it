98c67131e8facc59173902af01d2688672b7b546
Nan Zhong <nan@nine27.com>
2012-12-10 11:38:55 -0500

    Add specific page titles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,6 @@ class UsersController < ApplicationController
   end
 
   def show
-
   end
 
   private
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def title(page_title)
+    content_for :title, "refactr.it - #{page_title.to_s}"
+  end
 end
