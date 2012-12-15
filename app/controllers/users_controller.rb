a584e84df11e6b160c08be6ece30056110a7acf1
Nan Zhong <nan@nine27.com>
2012-12-14 22:03:17 -0500

    Add basic activity feed to user profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,15 @@ class UsersController < ApplicationController
   end
 
   def show
+    @activities = []
+
+    @activities += @user.problems
+    @activities += @user.solutions
+
+    @activities += Problem.where(:'comments.user_id' => @user.id).map {|p| p.comments.where(:user_id => @user.id)}.flatten
+    @activities += Solution.where(:'comments.user_id' => @user.id).map {|s| s.comments.where(:user_id => @user.id)}.flatten
+
+    @activities.sort! {|a, b| a.created_at <=> b.created_at}
   end
 
   private
