14acf12c7574c38e3c7a832d8b034d6afe05d430
Nan Zhong <nan@nine27.com>
2012-12-14 22:15:39 -0500

    Ignore redis connection exceptions for voting

--- lib/cache.rb
+++ lib/cache.rb
@@ -4,21 +4,36 @@ class Cache
   DOWN_VOTE = "down_vote"
 
   def self.voted_on_problem?(user_key, problem)
-    voted = REDIS.get(self.problem_vote_key(user_key, problem))
-    voted.blank? ? false : voted
+    begin
+      voted = REDIS.get(self.problem_vote_key(user_key, problem))
+      voted.blank? ? false : voted
+    rescue Redis::CannotConnectError
+      false
+    end
   end
 
   def self.vote_on_problem(user_key, problem, vote)
-    REDIS.set(self.problem_vote_key(user_key, problem), vote)
+    begin
+      REDIS.set(self.problem_vote_key(user_key, problem), vote)
+    rescue Redis::CannotConnectError
+    end
   end
 
   def self.voted_on_solution?(user_key, solution)
-    voted = REDIS.get(self.solution_vote_key(user_key, solution))
-    voted.blank? ? false : voted
+    begin
+      voted = REDIS.get(self.solution_vote_key(user_key, solution))
+      voted.blank? ? false : voted
+    rescue Redis::CannotConnectError
+      false
+    end
   end
 
   def self.vote_on_solution(user_key, solution, vote)
-    REDIS.set(self.solution_vote_key(user_key, solution), vote)
+    begin
+      REDIS.set(self.solution_vote_key(user_key, solution), vote)
+    rescue Redis::CannotConnectError
+      false
+    end
   end
 
   def self.problem_vote_key(user_key, problem)
